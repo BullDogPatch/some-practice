a1b85d6f3e66d9496142fcb20b611d732e2b95c4
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-10-20 17:02:18 +0100

    have a way to sort articles by date, comment_count and votes in order. Fetch comments function added even though no use for it yet

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,19 +1,52 @@
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { fetchArticles } from '../utils/api'
+import { fetchArticles, fetchAllArticles } from '../utils/api'
 import Article from './Article'
 import Loading from './Loading'
 
 const Articles = () => {
   const { data, isLoading, isError } = useQuery(['articles'], fetchArticles)
+  // fetchAll Articles takes a sortBy and orderBy query parameter
+  // const [sortBy, setSortBy] = useState('created_at')
+  // const [orderBy, setOrderBy] = useState('desc')
+  // const { data, isLoading, isError } = useQuery(
+  //   ['articles', { sortBy, orderBy }],
+  //   fetchAllArticles
+  // )
+
+  console.log(data)
 
   if (isLoading) return <Loading />
 
   return (
-    <ul>
-      {data?.map(article => (
-        <Article key={article.article_id} article={article} />
-      ))}
-    </ul>
+    <>
+      {/* <div className="sort-container">
+        <label htmlFor="sort_by">Sort: </label>
+        <select
+          id="sort_by"
+          value={sortBy}
+          onChange={e => setSortBy(e.target.value)}
+        >
+          <option value="created_at">Date</option>
+          <option value="comment_count">Comment Count</option>
+          <option value="votes">Votes</option>
+        </select>
+        <label htmlFor="order">By: </label>
+        <select
+          id="order"
+          value={orderBy}
+          onChange={e => setOrderBy(e.target.value)}
+        >
+          <option value="desc">Descending</option>
+          <option value="asc">Ascending</option>
+        </select>
+      </div> */}
+      <ul>
+        {data?.map(article => (
+          <Article key={article.article_id} article={article} />
+        ))}
+      </ul>
+    </>
   )
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -33,3 +33,20 @@ export const patchArticleById = async (id, value) => {
   })
   return data
 }
+
+/* API calls with functionality to be able to order by comments, votes and by date and be to sort by ascending and descending order */
+
+//  https://backend-news-example.herokuapp.com/api/articles?sort_by=comment_count&order=desc
+export const fetchAllArticles = async (sortBy, orderBy) => {
+  const { data } = await axios.get(
+    `${baseUrl}/articles?sort_by=${sortBy}&order=${orderBy}`
+  )
+  return data.articles
+}
+
+/* Fetch comments by article ID */
+
+export const fetchCommentsForArticle = async article_id => {
+  const { data } = await axios.get(`${baseUrl}/articles/${article_id}/comments`)
+  return data.comments
+}

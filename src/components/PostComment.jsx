5e8f46c6da301ca3e9698b9d4cb3598f0b0477a9
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-10-29 21:46:07 +0100

    tiny bit done

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import { postComment } from '../utils/api'
 
 const PostComment = ({ article_id, commentTotal, setCommentTotal }) => {
   const [text, setText] = useState('')
 
+  const handleChange = e => {
+    setText(e.target.value)
+  }
   /* needs a form with label, div and textarea. import postComment function from api */
-  return <></>
+  return (
+    <form>
+      <textarea value={text} onChange={handleChange} cols="5"></textarea>
+    </form>
+  )
 }
 
 export default PostComment
